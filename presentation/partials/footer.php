9b6880f651098c7a63a4764a01ef2f3542ff8d16
Uditha Landekumbura <uditha@programmer.net>
2025-07-23 12:20:50 +0530

    footer componet

---
+++ presentation/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-5 py-3 text-center text-muted">
+    <div class="container">
+        <p class="mb-0">
+            Task Manager - developed at BMI Campus &copy; <?= date('Y') ?> |
+            <a href="https://github.com/uditha1994">Uditha Landekumbura</a>
+        </p>
+    </div>
+</footer>
